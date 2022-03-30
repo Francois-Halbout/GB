62009c04b3be54e401a22d5886e6ad50499a1391
Yanncho <yann.chove@epita.fr>
2022-03-30 16:09:04 +0200

    ok

---
+++ GunBattle/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,14 @@
+public class MainMenu : MonoBehaviour
+{
+    void LOAD_SCENE()
+    {
+        Application.LoadLevel("SampleScene");
+    }
+
+
+    void QUIT_GAME()
+    {
+        Application.Quit();
+    }
+}
+
